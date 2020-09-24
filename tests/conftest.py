9947f5168955eaed3098235aac60a9972e896dc9
Dan Egnor <Dan Egnor>
2020-09-23 17:40:20 -0700

    more progress toward test for ThreadManager

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,8 +11,8 @@ import pytest
 from escape_roomba.context import Context
 
 #
-# Mock generators as pytest *factory fixtures*:
-# https://docs.pytest.org/en/stable/fixture.html#factories-as-fixtures
+# Mock generators as pytest *factory fixtures*
+# (see docs.pytest.org/en/stable/fixture.html#factories-as-fixtures).
 # These are used by referencing the name in a test function parameters:
 #
 # def test_some_stuff(make_context):
@@ -24,38 +24,44 @@ from escape_roomba.context import Context
 def make_discord_id():
     """Factory fixture to generate unique Discord/snowflake-style ID ints."""
 
-    last_id = 0x9305ac7cb9a2038  # 2020-01-01 at midnight.
+    last_id = 0x92ee70e00000000  # 2020-01-01 at midnight.
 
-    def _factory():
+    def factory():
         nonlocal last_id
         last_id += (1000 << 22)  # Advance one second.
         return last_id
 
-    return _factory
+    return factory
 
 
 @pytest.fixture
-def make_context(
-        mocker, event_loop,
-        make_discord_guild, make_discord_member, make_discord_text_channel):
+def make_context(mocker, event_loop,
+                 make_discord_guild, make_discord_member,
+                 make_discord_text_channel, make_discord_message):
     """Factory fixture to generate escape_roomba.Context-like Mock objects,
     including a discord.Client-like Mock."""
 
-    def _factory(guild_count=0, member_count=0, channel_count=0):
+    def factory(guild_count=0, member_count=0, channel_count=0,
+                message_count=0):
         context = Context()
         context.logger = logging.getLogger('test')  # TODO: Mock if needed.
         context.client = mocker.Mock(spec=discord.Client)
         context.client.guilds = []
         for gi in range(guild_count):
-            guild = make_discord_guild(name=f'Mock Guild {gi}')
-            guild.members = [
-                make_discord_member(guild, name=f'Mock Member {mi}',
-                                    discriminator=1000 + mi)
-                for mi in range(member_count)]
-            guild.channels = [
-                make_discord_text_channel(guild, name=f'mock-channel-{ci}')
-                for ci in range(channel_count)]
-            context.client.guilds.append(guild)
+            g = make_discord_guild(name=f'Mock Guild {gi}')
+            context.client.guilds.append(g)
+
+            for mi in range(member_count):
+                g.members.append(make_discord_member(
+                    g, name=f'Mock Member {mi}', discriminator=1000 + mi))
+            for ci in range(channel_count):
+                c = make_discord_text_channel(g, name=f'mock-channel-{ci}')
+                g.channels.append(c)
+                for mi in range(message_count):
+                    assert member_count > 0  # need members
+                    c.test_history.append(make_discord_message(
+                        g, c, g.members[mi % member_count],
+                        f'Mock message {mi} in #mock-channel-{ci}'))
 
         gs = context.client.guilds
         context.client.get_guild.side_effect = lambda id: next(
@@ -66,29 +72,29 @@ def make_context(
             (m for g in gs for m in g.members if m.id == id), None)
         return context
 
-    return _factory
+    return factory
 
 
 @pytest.fixture
 def make_discord_guild(mocker, make_discord_id):
     """Factory fixture to generate discord.Guild-like Mock objects."""
 
-    def _factory(name='Mock Guild'):
-        guild = mocker.MagicMock(spec=discord.Guild)
+    def factory(name='Mock Guild'):
+        guild = mocker.Mock(spec=discord.Guild)
         guild.id = make_discord_id()
         guild.name = name
         guild.channels = []
         guild.members = []
         return guild
 
-    return _factory
+    return factory
 
 
 @pytest.fixture
 def make_discord_member(mocker, make_discord_id):
     """Factory fixture to generate discord.Member-like Mock objects."""
 
-    def _factory(guild, name='Mock Member', discriminator='1234'):
+    def factory(guild, name='Mock Member', discriminator='1234'):
         member = mocker.Mock(spec=discord.Member)
         member.id = make_discord_id()
         member.guild = guild
@@ -96,36 +102,47 @@ def make_discord_member(mocker, make_discord_id):
         member.discriminator = discriminator
         return member
 
-    return _factory
+    return factory
 
 
 @pytest.fixture
 def make_discord_text_channel(mocker, make_discord_id):
     """Factory fixture to generate discord.TextChannel-like Mock objects."""
 
-    def _factory(guild, name='mock-channel'):
+    def factory(guild, name='mock-channel'):
         channel = mocker.Mock(spec=discord.TextChannel)
         channel.id = make_discord_id()
         channel.guild = guild
         channel.type = discord.ChannelType.text
         channel.name = name
         channel.topic = f'topic for {name}'
+        channel.test_history = []
+
+        async def get_history(limit=100, oldest_first=None):
+            limit = len(channel.test_history) if limit is None else limit
+            history = channel.test_history
+            slice = history[:limit] if oldest_first else history[:-limit:-1]
+            for m in slice:
+                yield m
+
+        channel.history.side_effect = get_history
         return channel
 
-    return _factory
+    return factory
 
 
 @pytest.fixture
 def make_discord_message(mocker, make_discord_id):
     """Factory fixture to generate discord.Message-like Mock objects."""
 
-    def _factory(guild, channel, author, content='Mock message content'):
+    def factory(guild, channel, author, content='Mock message content'):
         message = mocker.Mock(spec=discord.Message)
         message.id = make_discord_id()
         message.guild = guild
         message.channel = channel
         message.author = author
         message.content = content
+        message.reactions = []
         return message
 
-    return _factory
+    return factory
--- tests/thread_manager_test.py
+++ tests/thread_manager_test.py
@@ -11,12 +11,12 @@ from escape_roomba.thread_manager import ThreadManager
 
 
 @pytest.mark.asyncio
-async def test_thread_creation(mocker, make_context):
+async def test_thread_creation(mocker, make_context, make_discord_message):
     # Create a Context with a discord.Client-like Mock.
-    context = make_context(guild_count=2, channel_count=2, member_count=2)
+    context = make_context(guild_count=2, channel_count=2, member_count=2,
+                           message_count=2)
 
     # Create the ThreadManager and register its listeners.
     ThreadManager(context)
 
-    # TODO: Finish this!
-    # await context.client.on_ready()
+    await context.client.on_ready()
